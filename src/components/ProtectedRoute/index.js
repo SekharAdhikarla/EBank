a59b1366391b575ed29066b81af77a319c58c748
Your Name <you@example.com>
2023-07-04 00:56:39 +0000

    commit message

---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+import React from 'react'
+import {Redirect, Route} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+const ProtectedRoute = props => {
+  const JwtToken = Cookies.get('jwt_token')
+  if (JwtToken === undefined) {
+    return <Redirect to="/ebank/login" />
+  }
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
